05a7195dc1db90fbcd2b1fb2e520738d88db2b7f
cngjsrnr <cngjsrnr@naver.com>
2020-10-10 18:27:02 +0900

    Feat Login UI
    로그인 UI 추가

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,33 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styled,{css} from "styled-components";
+import styled, { css } from "styled-components";
 
 const flexCenterAlign = css`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 `;
 
 const HeaderLabel = styled.div`
   border-bottom: solid;
   height: 40px;
-  width: 100%;
+  width: 96%;
+  margin-left: 2%;
+  margin-right: 2%;
   ${flexCenterAlign}
 `;
 
 const LogoLabel = styled.span`
+  margin-left: 10px;
   font-size: 20px;
   text-align: left;
-
 `;
 const AuthLabel = styled.span`
   font-size: 15px;
   text-align: right;
   justify-self: flex-end;
-
+  margin-right: 10px;
 `;
+
 const EmptyLabel = styled.span`
   flex: 1;
+  height:1px;
+`;
+
+const LoginLabel=styled.span`
+  margin-right:10px;
 `
+
+const RegisterLabel=styled.span`
+  margin-left:10px;
+`
+
+
 
 const Header = () => {
   return (
@@ -39,13 +53,16 @@ const Header = () => {
       </LogoLabel>
       <EmptyLabel></EmptyLabel>
       <AuthLabel>
+        <LoginLabel>
         <Link style={{ textDecoration: "none" }} to="/login">
           로그인
         </Link>
-        <span> </span>
+        </LoginLabel>
+        <RegisterLabel>
         <Link style={{ textDecoration: "none" }} to="/register">
           회원가입
         </Link>
+        </RegisterLabel>
       </AuthLabel>
     </HeaderLabel>
   );
--- front/src/components/LoginForm.js
+++ front/src/components/LoginForm.js
@@ -1,5 +1,48 @@
 import React from "react";
 
+import styled, { css } from "styled-components";
+
+const LoginLabel = styled.div`
+  display: flex;
+  height: 85vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StateLabel =styled.div`
+  text-align: left;
+  width:330px;
+  font-size: 18px;
+  font-weight: 900;
+`;
+
+const UsernameInput = styled.input`
+  width:320px;
+  height: 50px;
+  margin:4px;
+  font-size:15px;
+`;
+
+const PasswordInput = styled.input`
+  width:320px;
+  height: 50px;
+  margin:4px;
+  font-size:15px;
+  
+`
+
+const LoginButton =styled.a`
+  display: flex;
+  width:200px;
+  height: 40px;
+  margin-top:30px;
+  background-color: gray;
+  align-items:center;
+  justify-content:center;
+`;
+
+
 const LoginForm = ({
   username,
   password,
@@ -8,38 +51,33 @@ const LoginForm = ({
   handleSubmit,
   validate,
 }) => {
-  
-  const handleKeyPress = (e) =>{
-    if(e.charCode==13){//엔터치면 로그인되게 함
+  const handleKeyPress = (e) => {
+    if (e.charCode == 13) {
+      //엔터치면 로그인되게 함
       handleSubmit();
     }
-  }
-
-
+  };
 
   return (
-    <div className="Login">
-     
-        아이디
-        <input
-          type="text"
-          name="username"
-          value={username}
-          onChange={handleChangeUsername}
-        />
-        <br/>
-        비밀번호
-        <input
-          type="password"
-          value={password}
-          onChange={handleChangePassword}
-          onKeyPress = {handleKeyPress}
-        />
-        <a onClick={handleSubmit}>
-          로그인
-        </a>
-  
-    </div>
+    <LoginLabel>
+      <StateLabel>로그인</StateLabel>
+      
+      <UsernameInput
+        type="text"
+        name="username"
+        value={username}
+        placeholder="USERNAME"
+        onChange={handleChangeUsername}
+      />
+      <PasswordInput
+        type="password"
+        value={password}
+        placeholder="PASSWORD"
+        onChange={handleChangePassword}
+        onKeyPress={handleKeyPress}
+      />
+      <LoginButton onClick={handleSubmit}>로그인 하기</LoginButton>
+    </LoginLabel>
   );
 };
 
