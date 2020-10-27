5cef41dab8b9bea085001300307c73d7270fabc1
ys <emost22@naver.com>
2020-10-27 10:09:30 +0900

    Fix routing

--- backend/yam.js
+++ backend/yam.js
@@ -8,6 +8,8 @@ var create = require('./routes/user_create');
 var main = require('./routes/main');
 var roomnumber = require('./routes/roomnumber');
 var roomlist = require('./routes/roomlist');
+var roominchk = require('./routes/roominchk');
+var roomin = require('./routes/roomin');
 const socketio = require('socket.io');
 const server = require('http').createServer(app);
 const io = socketio.listen(server);
@@ -23,14 +25,16 @@ app.post('/user_create', create.create);
 app.post('/loginchk', main.main);
 app.post('/roomnumber', roomnumber.roomnumber);
 app.post('/roomlist', roomlist.roomlist);
+app.post('/roominchk', roominchk.roominchk);
+app.post('/roomin', roomin.roomin);
 
-// 클라이언트가 접속했을 때의 이벤트 설정 --- (※4)
+// 클라이언트가 접속했을 때의 이벤트 설정
 io.on('connection', (socket) => {
     console.log('사용자 접속:', socket.client.id);
-    // 메시지를 받으면 --- (※5)
+    // 메시지를 받으면
     socket.on('chat-msg', (msg) => {
         console.log('message:', msg);
-        // 모든 클라이언트에게 전송 --- (※6)
+        // 모든 클라이언트에게 전송
         io.emit('chat-msg', msg);
     });
 });
