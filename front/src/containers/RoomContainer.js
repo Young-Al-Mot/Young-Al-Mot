ab65b69aa25f5faf6d99c42b4588106cd91edc4e
cngjsrnr <cngjsrnr@naver.com>
2020-11-02 22:57:30 +0900

    Fix roomout

--- front/src/containers/RoomContainer.js
+++ front/src/containers/RoomContainer.js
@@ -118,17 +118,14 @@ const RoomContainer = () => {
 
   //마운트 되었을때
   useEffect(() => {
-    window.history.pushState(null, null, window.location.href);
+    window.history.pushState(null, null, window.location.href);//두로가기 막음
     window.onpopstate = function () {
       history.go(1);
     };
-    window.onunload = function () {
-      console.log("aa");
-      dispatch(roomOutRequest(room.roomid)).then(()=>{
-        console.log("bb");
-      });
+    window.onbeforeunload = function () {
+      winUnload();
     };
-    window.onkeydown = logKey;
+    window.onkeydown = logKey;//ctrl+w누르면 방 나가지게
     function logKey(e) {
       if (e.ctrlKey && e.key === "w") {
         dispatch(roomOutRequest(room.roomid));
