c4868738a82258c6cd2688facf9dc68272e9bf0a
cngjsrnr <cngjsrnr@naver.com>
2020-11-04 15:05:35 +0900

    Fix socket

---
+++ front/src/socket/SocketFunc.js
@@ -0,0 +1,17 @@
+import React from "react";
+import socketio from "socket.io-client";
+
+const socket = socketio.connect("http://localhost:5000");
+
+export const socketIn = (roomid,nickname) => {
+  //방에 들어가게되면 소켓으로 방 접속한걸 알림
+  socket.emit("join", {
+    roomno: roomid,
+    name: nickname,
+  });
+};
+
+export const getSocket = () => {
+  return socket;
+};
+
