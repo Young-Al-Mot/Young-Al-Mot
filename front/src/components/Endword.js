7cf08496a77b6ca2cfd8e825eda381c7cecd0752
bluenight12 <ekehd258@gmail.com>
2020-11-09 14:30:29 +0900

    Feat Progress Bar

--- front/src/components/Endword.js
+++ front/src/components/Endword.js
@@ -18,14 +18,16 @@ const TopContent = styled.div`
   align-items: center;
   margin-top: 10px;
   height: 10%;
+  font-size: 45px;
+  font-family: sans-serif;
 `;
-const TopTopContent = styled.div`
-  border: solid thin;
+const TopTopContent = styled.div` 
+  margin-top:20px;
+  padding-top:20px;
 `;
 const TopBotContent = styled.div`
   height: 25px;
   font-size: 100%;
-  border: solid thin;
   margin-top: 10px;
   text-align: center;
 `;
@@ -34,6 +36,7 @@ const MidContnet = styled.div`
   width: 85%;
   border: solid thin;
   font-size: 200%;
+  text-align: center;
 `;
 const BotContent = styled.div`
   margin-bottom: 10px;
@@ -43,26 +46,26 @@ const BotContent = styled.div`
 const ProgressBarWrapper = styled.div`
   margin: 30px;
   border: 2px solid lightblue;
-  height: 30vh;
-  width: 50vw;
+  height: 5vh;
+  width: 60vw;
 `;
 const fill = keyframes`
-  100% {width: 0%}
-  0% {width: 100%} 
+  0% {width: 0%}
+  100% {width: 100%} 
 `;
 
 const ProgressBar = styled.div`
   background: lightblue;
   height: 100%;
-  animation: ${fill} 5s linear 20; //will animate 20 times
-  animation-play-state: ${props=>props.play};
+  animation: ${fill} 5s linear;
+  animation-duration: 1;
+  animation-direction: reverse;
+  animation-fill-mode: forwards;
 `;
 
 const socket = getSocket();
 
-const Endword = ({ message, word, startWord, round,timer,settimer }) => {
-
-  
+const Endword = ({ message, word, startWord, round, timer, settimer }) => {
   useEffect(() => {
     socket.off("gametime");
     socket.on("gametime", (time) => {
@@ -71,19 +74,20 @@ const Endword = ({ message, word, startWord, round,timer,settimer }) => {
     });
   });
 
-  const timerBar=()=>{
-
-    if(timer<=5&&timer>0){
+  const timerBar = () => {
+    if (timer <= 5 && timer > 0) {
       console.log("time5");
-    return (<ProgressBarWrapper>
-      <ProgressBar />
-    </ProgressBarWrapper>)
-    }
-    else if(timer=="시간초과"){
+      return (
+        <ProgressBarWrapper>
+          <ProgressBar/>
+        </ProgressBarWrapper>
+      );
+    } else if (timer == 0) {
       console.log("time0");
-      return(<ProgressBarWrapper>
-        <ProgressBar play="Paused"/>
-      </ProgressBarWrapper>)
+      return (
+        <ProgressBarWrapper>
+        </ProgressBarWrapper>
+      );
     }
   };
 
@@ -94,9 +98,7 @@ const Endword = ({ message, word, startWord, round,timer,settimer }) => {
           {startWord}
           {/* 라운드에 해당하는 글짜를 크게표시하거나 색을 다르게 표시해야함 */}
         </TopTopContent>
-        <TopBotContent>
-          {timerBar()}
-        </TopBotContent>
+        <TopBotContent>{timerBar()}</TopBotContent>
       </TopContent>
       <MidContnet>
         {word[word.length - 1]}
